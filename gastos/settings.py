c8a61d490d6e305e712160af15b26c2763c8908f
diogenes <diogenes.araujo.palla@gmail.com>
2022-08-10 01:50:39 -0300

    DB Desenvolvimento mudado pra sqlite

--- gastos/settings.py
+++ gastos/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 
-import dj_database_url
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,11 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = False
+DEBUG = True
+#DEBUG = False
 
-ALLOWED_HOSTS = ['gastos-pessoais.herokuapp.com']
-#ALLOWED_HOSTS = ['*']
+#ALLOWED_HOSTS = ['gastos-pessoais.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -80,7 +78,7 @@ WSGI_APPLICATION = 'gastos.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -88,16 +86,7 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gastos',
-        'USER': 'postgres',
-        'PASSWORD': 'philoDio',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+
 """
 DATABASES = {
     'default': {
@@ -109,6 +98,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+"""
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
